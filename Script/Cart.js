6bc7686947feb04f23d3dc79025a15171bee83b1
Giaugg <giaule0111@gmail.com>
2023-10-27 13:55:35 +0700

    fix bug Object

--- Script/Cart.js
+++ Script/Cart.js
@@ -1,44 +1,36 @@
-
-
 var main_data;
+var objects; // Khai báo biến objects ở ngoài để truy cập sau này
+
 var Read_file_user = function () {
-
-// Đọc nội dung của file1.txt
-    fetch('../filedata/cart_for_user.txt') 
-    .then(response => response.text())
-    .then(data => { 
-        main_data = data; // In nội dung của file văn bản vào console
-        console.log(main_data)
-        getObjects()
-    })
-    .catch(error => {
-        console.error(error);   
-
-});
+    // Đọc nội dung của file1.txt
+    fetch('../filedata/cart_for_user.txt')
+        .then(response => response.text())
+        .then(data => {
+            main_data = data; // Lưu dữ liệu vào biến main_data
+            console.log(main_data);
+            getObjects();
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
-var getObjects = function(){
-
+var getObjects = function () {
     const lines = main_data.split('\n'); // Tách tệp thành từng dòng
 
-    const objects = lines.map(line => {
-        const [productId,
+    objects = lines.map(line => {
+        const [productId, brand, img, name, price] = line.split(' '); // Tách dấu cách
+        return {
+            productId,
             brand,
             img,
             name,
-            price] = line.split(' '); // Tách dấu cách
-            return {productId,
-                brand,
-                img,
-                name,
-                price: parseInt(price, 10) };
-        });
-    
-        console.log(objects);
-        // console.log(objects[1]);
+            price: parseInt(price, 10)
+        };
+    });
+
+    // console.log(objects[1]);
 }
 
-
 Read_file_user();
-
-
+console.log(objects); // Đây là nơi bạn có thể truy cập biến objects
