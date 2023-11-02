4182c1e62809187146e044eb676e2400dddfee1b
Giaugg <giaule0111@gmail.com>
2023-11-02 23:08:59 +0700

    show cart (done)

--- Script/Cart.js
+++ Script/Cart.js
@@ -8,28 +8,27 @@ var Read_file_user = function () {
         .then(data => {
             main_data = data; // Lưu dữ liệu vào biến main_data
             console.log(main_data);
-            getObjects();
+            getobjects();
         })
         .catch(error => {
             console.error(error);
         });
 }
 
-var getObjects = function () {
+var getobjects = function () {
     const lines = main_data.split('\n'); // Tách tệp thành từng dòng
 
     objects = lines.map(line => {
-        const [productId, brand, img, name, price] = line.split(' '); // Tách dấu cách
+        const [productId, brand, name, price] = line.split(' '); // Tách dấu cách
         return {
             productId,
             brand,
-            img,
             name,
-            price: parseInt(price, 10)
+            price
         };
     });
-
-    // console.log(objects[1]);
+    console.log(objects)
+    create_user_cart();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -38,7 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const sticket = document.getElementById("cart-user-modal");
     const closeButtons = document.getElementById("close-logo");
 
-    console.log(closeButtons)
 
     // Sự kiện khi nhấn nút "Hiển thị Sticket" hoặc dấu x
     toggleSticketButton.addEventListener("click", function() {
@@ -46,11 +44,40 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     closeButtons.addEventListener("click", function() {
-            sticket.style.display = "none"; // Ẩn Sticket
-        });
-    
+        sticket.style.display = "none"; // Ẩn Sticket
+    });
 });
 
+var create_user_cart = function(){
+    const itemContainer = document.getElementById("item-list");
+
+    objects.forEach(function(objects,index) {
+        // Tạo một phần tử .item
+        const itemElement = document.createElement("div");
+        itemElement.classList.add("item");
+
+        const originalString = "ID_Products001.jpg";
+        const modifiedString = originalString.replace(/001/g, (index+1).toString().padStart(3, "0"));
+
+        const charname = objects.name;
+        const name = charname.replace('%', ' ');
+        console.log(name);
+        // Tạo các phần tử con và đặt giá trị từ đối tượng sản phẩm
+        itemElement.innerHTML = `
+            <input type="checkbox">
+            <img src=".\\Images\\products\\main_products\\${modifiedString}" alt="">
+            <p>${name}</p>
+            <p>${objects.price}</p>
+            <div>
+                <div> số lượng</div>
+                <input type="number" id="myNumber" value= 1>
+            </div>
+            <p>${objects.total_price}</p>
+        `;
+
+        // Thêm phần tử .item vào phần tử gốc
+        itemContainer.appendChild(itemElement);
+    });
+}
 
 Read_file_user();
-console.log(objects); // Đây là nơi bạn có thể truy cập biến objects
