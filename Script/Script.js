59fa6cc158d920284f30495529bc65374eba7b90
Giaugg <giaule0111@gmail.com>
2023-11-03 13:48:33 +0700

    a

--- Script/Script.js
+++ Script/Script.js
@@ -16,3 +16,34 @@ function scrollSnapAutoScroll() {
 }
 
 scrollSnapAutoScroll();
+////////////// CREATE MAIN DATA////////////
+var Read_file_maindata = function () {
+    // Đọc nội dung của file1.txt
+    fetch('../filedata/main_data.txt')
+        .then(response => response.text())
+        .then(data => {
+            main_data = data; // Lưu dữ liệu vào biến main_data
+            console.log(main_data);
+            getobjects();
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+var getobjects = function () {
+    const lines = main_data.split('\n'); // Tách tệp thành từng dòng
+
+    objects = lines.map(line => {
+        const [productId, brand, name, price, sl] = line.split(' '); // Tách dấu cách
+        return {
+            productId,
+            brand,
+            name,
+            price,
+            sl
+        };
+    });
+    console.log(objects)
+    create_user_cart();
+}
