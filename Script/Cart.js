610e4e79042f60d46483fb2c0ebcca8dffb81ef4
Giaugg <giaule0111@gmail.com>
2023-11-08 01:03:39 +0700

    format

--- Script/Cart.js
+++ Script/Cart.js
@@ -2,61 +2,61 @@ var main_data;
 var objects; // Khai báo biến objects ở ngoài để truy cập sau này
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Lấy các phần tử HTML
-  const toggleSticketButton = document.getElementById("show-cart");
-  const sticket = document.getElementById("cart-user-modal");
-  const closeButtons = document.getElementById("close-logo");
+	// Lấy các phần tử HTML
+	const toggleSticketButton = document.getElementById("show-cart");
+	const sticket = document.getElementById("cart-user-modal");
+	const closeButtons = document.getElementById("close-logo");
 
-  // Sự kiện khi nhấn nút "Hiển thị Sticket" hoặc dấu x
-  toggleSticketButton.addEventListener("click", function () {
-    sticket.style.display = "flex"; // Hiển thị Sticket
-    Read_data_cart();
-  });
-  
+	// Sự kiện khi nhấn nút "Hiển thị Sticket" hoặc dấu x
+	toggleSticketButton.addEventListener("click", function () {
+		sticket.style.display = "flex"; // Hiển thị Sticket
+		Read_data_cart();
+	});
 
-  closeButtons.addEventListener("click", function () {
-    sticket.style.display = "none"; // Ẩn Sticket
-    const divElement = document.getElementById("item-list");
-    while (divElement.firstChild) {
-      divElement.removeChild(divElement.firstChild);
-    }
-    reset_price();
-  });
+	closeButtons.addEventListener("click", function () {
+		sticket.style.display = "none"; // Ẩn Sticket
+		const divElement = document.getElementById("item-list");
+		while (divElement.firstChild) {
+			divElement.removeChild(divElement.firstChild);
+		}
+		reset_price();
+	});
 });
 
 var Read_data_cart = function () {
-  var cart = JSON.parse(localStorage.getItem("cart"));
-  // console.log(cart);
-  objects = cart;
-  create_user_cart();
-  console.log(objects);
+	var cart = JSON.parse(localStorage.getItem("cart"));
+	// console.log(cart);
+	objects = cart;
+	create_user_cart();
+	console.log(objects);
 };
 
 var create_user_cart = function () {
-  const itemContainer = document.getElementById("item-list");
-  if (objects.length === 0) empty_cart();
-  else {
-    objects.forEach(function (objects, index) {
-      // Tạo một phần tử .item
-      const itemElement = document.createElement("div");
-      itemElement.classList.add("item");
-      const id = objects.id;
-      const originalString = "ID_Products001.jpg";
-      const modifiedString = originalString.replace(
-        /001/g,
-        id.toString().padStart(3, "0")
-      );
+	const itemContainer = document.getElementById("item-list");
+	if (objects.length === 0) empty_cart();
+	else {
+		objects.forEach(function (objects, index) {
+			// Tạo một phần tử .item
+			const itemElement = document.createElement("div");
+			itemElement.classList.add("item");
+			const id = objects.id;
+			const originalString = "ID_Products001.jpg";
+			const modifiedString = originalString.replace(
+				/001/g,
+				id.toString().padStart(3, "0")
+			);
 
-      const charname = objects.name;
-      const name = charname.replace(/%/g, " ");
-      // console.log(name);
+			const charname = objects.name;
+			const name = charname.replace(/%/g, " ");
+			// console.log(name);
 
-      const total_price = parseInt(objects.price) * parseInt(objects.quality);
-      // Tạo các phần tử con và đặt giá trị từ đối tượng sản phẩm
+			const total_price =
+				parseInt(objects.price) * parseInt(objects.quality);
+			// Tạo các phần tử con và đặt giá trị từ đối tượng sản phẩm
 
-      // console.log(objects)
+			// console.log(objects)
 
-      itemElement.innerHTML = `
+			itemElement.innerHTML = `
                 <input type="checkbox" >
                 <img src=".\\Images\\products\\${modifiedString}" alt="" class="item_img">
                 <div>
@@ -81,97 +81,99 @@ var create_user_cart = function () {
                 </div>
             `;
 
-      // Thêm phần tử .item vào phần tử gốc
-      itemContainer.appendChild(itemElement);
-    });
-  }
-  delete_item();
-  calc_money();
+			// Thêm phần tử .item vào phần tử gốc
+			itemContainer.appendChild(itemElement);
+		});
+	}
+	delete_item();
+	calc_money();
 };
 
-// Read_file_user();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////// -->
 
 var delete_item = function () {
-  delete_HTML_item();
-  delete_data_item();
+	delete_HTML_item();
+	delete_data_item();
 };
 
 var delete_HTML_item = function () {
-  const alldeletebutton = document.querySelectorAll(".delete-item");
+	const alldeletebutton = document.querySelectorAll(".delete-item");
 
-  alldeletebutton.forEach(function (btn) {
-    btn.addEventListener("click", function () {
-      btn.parentElement.remove();
-    });
-  });
+	alldeletebutton.forEach(function (btn) {
+		btn.addEventListener("click", function () {
+			btn.parentElement.remove();
+		});
+	});
 };
-
+  
 var delete_data_item = function () {
-  const alldeleteitem = document.querySelectorAll(".item");
-  alldeleteitem.forEach((item) => {
-    btn = item.querySelector(".delete-item");
-    // console.log(btn);
-    btn.addEventListener("click", function () {
-      var cart = JSON.parse(localStorage.getItem("cart"));
+	const alldeleteitem = document.querySelectorAll(".item");
+	alldeleteitem.forEach((item) => {
+		btn = item.querySelector(".delete-item");
+		// console.log(btn);
+		btn.addEventListener("click", function () {
+			var cart = JSON.parse(localStorage.getItem("cart"));
 
-      const target = item.querySelector("#item-ID");
+			const target = item.querySelector("#item-ID");
 
-      const newcart = cart.filter(function (cart) {
-        return parseInt(cart.id) !== parseInt(target.textContent);
-      });
+			const newcart = cart.filter(function (cart) {
+				return parseInt(cart.id) !== parseInt(target.textContent);
+			});
 
-      if (newcart.length === 0) empty_cart();
+			if (newcart.length === 0) empty_cart();
 
-      // console.log(target.textContent, newcart);
-      localStorage.setItem("cart", JSON.stringify(newcart));
-    });
-  });
+			// console.log(target.textContent, newcart);
+			localStorage.setItem("cart", JSON.stringify(newcart));
+		});
+	});
 };
 
 var calc_money = function () {
-  const allcheckbox = document.querySelectorAll(".item");
-  // console.log(allcheckbox)
-  allcheckbox.forEach((div) => {
-    // console.log(div)
-    div.addEventListener("click", function () {
-      const element = div.querySelector('input[type="checkbox"]'); // Lấy phần tử input có type là "text"
-      element.checked = !element.checked;
+	const items = document.querySelectorAll(".item");
+	// console.log(item)
+	items.forEach((div) => {
+		// console.log(div)
+		div.addEventListener("click", function () {
+			const element = div.querySelector('input[type="checkbox"]'); // Lấy phần tử input có type là "text"
 
-      let price = div.querySelector("#total-price");
-      let num = div.querySelector("#n");
-      calc(price, num, element);
-    });
-  });
+			element.checked = !element.checked;
+
+			let price = div.querySelector("#total-price");
+			let num = div.querySelector("#n");
+			calc(price, num, element);
+		});
+	});
 };
 
 var calc = function (price, num, check) {
-  var total = document.getElementById("total-item");
-  var total_cost = document.getElementById("total-cost");
+	var total = document.getElementById("total-item");
+	var total_cost = document.getElementById("total-cost");
 
-  // console.log(price)
-  if (check.checked) {
-    // alert(total.textContent)
-    total.textContent = parseInt(total.textContent) + 1;
-    total_cost.textContent =
-      parseInt(total_cost.textContent) + parseInt(price.textContent);
-  } else {
-    total.textContent = parseInt(total.textContent) - 1;
-    total_cost.textContent =
-      parseInt(total_cost.textContent) - parseInt(price.textContent);
-  }
+	// console.log(price)
+	if (check.checked) {
+		// alert(total.textContent)
+		total.textContent = parseInt(total.textContent) + 1;
+		total_cost.textContent =
+			parseInt(total_cost.textContent) + parseInt(price.textContent);
+	} else {
+		total.textContent = parseInt(total.textContent) - 1;
+		total_cost.textContent =
+			parseInt(total_cost.textContent) - parseInt(price.textContent);
+	}
 };
 var reset_price = function () {
-  var total = document.getElementById("total-item");
-  var total_cost = document.getElementById("total-cost");
-  total.textContent = 0;
-  total_cost.textContent = 0;
+	var total = document.getElementById("total-item");
+	var total_cost = document.getElementById("total-cost");
+	total.textContent = 0;
+	total_cost.textContent = 0;
 };
 
 var empty_cart = function () {
-  const itemContainer = document.getElementById("item-list");
-  var itemElementEmty = document.createElement("div");
-  itemElementEmty.innerHTML = `
+	const itemContainer = document.getElementById("item-list");
+	var itemElementEmty = document.createElement("div");
+	itemElementEmty.innerHTML = `
     <p id="empty"> Giỏ hàng trống xin vui lòng lựa chọn sản phẩm cần mua! </p> 
     `;
-  itemContainer.appendChild(itemElementEmty);
+	itemContainer.appendChild(itemElementEmty);
 };
--- Script/Script.js
+++ Script/Script.js
@@ -3,63 +3,64 @@ var main_data;
 var api = "http://localhost:3000/Item";
 
 function scrollSnapAutoScroll() {
-  const container = document.querySelector(".gallery");
-  const scrollItems = document.querySelectorAll(".gallery_item");
-  let currentIndex = 0;
+	const container = document.querySelector(".gallery");
+	const scrollItems = document.querySelectorAll(".gallery_item");
+	let currentIndex = 0;
 
-  function scrollToNextItem() {
-    currentIndex = (currentIndex + 1) % scrollItems.length;
-    container.scrollTo({
-      left: scrollItems[currentIndex].offsetLeft,
-      behavior: "smooth",
-    });
-  }
+	function scrollToNextItem() {
+		currentIndex = (currentIndex + 1) % scrollItems.length;
+		container.scrollTo({
+			left: scrollItems[currentIndex].offsetLeft,
+			behavior: "smooth",
+		});
+	}
 
-  setInterval(scrollToNextItem, 3000); // Tự động cuộn mỗi 3 giây (3000ms)
+	setInterval(scrollToNextItem, 3000); // Tự động cuộn mỗi 3 giây (3000ms)
 }
 
 scrollSnapAutoScroll();
 ////////////// CREATE MAIN DATA////////////
 
 var Read_file_maindata = function () {
-  fetch(api)
-    .then((response) => response.json())
-    .then(function (data) {
-      render(data);
-    });
+	fetch(api)
+		.then((response) => response.json())
+		.then(function (data) {
+			render(data);
+		});
 };
 
 function render(courses) {
-  var itemContainer = document.getElementById("flex-container");
+	var itemContainer = document.getElementById("flex-container");
 
-  var html = courses.map(function (course) {
-    // console.log(course.name)
-    return `<div class="flex-item">
+	var html = courses.map(function (course) {
+		// console.log(course.name)
+		return `<div class="flex-item">
             <div class="div-img"> 
-            
+               
             <img src="${course.imgage}" alt="" id="flex-image">
             </div>
-            <button class="add-to-cart-button" onclick="themvaogiohang(${course.id},'${course.name}',${course.price},'${course.imgage}')">add to cart</button>
+            <button class="Add-to-cart-button" onclick="themvaogiohang(${course.id},'${course.name}',${course.price},'${course.imgage}')">add to cart</button>
             <p id="name">${course.name}</p>
             <p class="price">$${course.price}</p>
             </div>`;
-  });
+	});
 
-  itemContainer.innerHTML += html.join("");
+	itemContainer.innerHTML += html.join("");
 }
 function themvaogiohang(id, ten, gia, hinh) {
-  var cart = JSON.parse(localStorage.getItem("cart"));
-  if (cart == null) {
-    console.log(1);
-    cart = [];
-    cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
-  } else {
-    let item = cart.find((item) => item.id === id);
-    if (item) item.quality++;
-    else cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
-  }
+	var cart = JSON.parse(localStorage.getItem("cart"));
+	if (cart == null) {
+		console.log(1);
+		cart = [];
+		cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
+	} else {
+		let item = cart.find((item) => item.id === id);
+		if (item) item.quality++;
+		else
+			cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
+	}
 
-  localStorage.setItem("cart", JSON.stringify(cart));
+	localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 Read_file_maindata();
@@ -79,218 +80,220 @@ const loginBtn = document.getElementById("login-btn");
 const home = document.getElementById("homeshow");
 
 showContainer.addEventListener("click", function () {
-  loginForm.style.display = "block";
-  home.style.display = "block";
+	loginForm.style.display = "block";
+	home.style.display = "block";
 });
 
 closeButtons1.addEventListener("click", function () {
-  loginForm.style.display = "none";
-  home.style.display = "none";
+	loginForm.style.display = "none";
+	home.style.display = "none";
 });
 closeButtons2.addEventListener("click", function () {
-  registerForm.style.display = "none";
-  home.style.display = "none";
+	registerForm.style.display = "none";
+	home.style.display = "none";
 });
 registerBtn.addEventListener("click", function () {
-  registerForm.style.display = "block";
-  loginForm.style.display = "none";
-  home.style.display = "block";
+	registerForm.style.display = "block";
+	loginForm.style.display = "none";
+	home.style.display = "block";
 });
 loginBtn.addEventListener("click", function () {
-  registerForm.style.display = "none";
-  loginForm.style.display = "block";
-  home.style.display = "block";
+	registerForm.style.display = "none";
+	loginForm.style.display = "block";
+	home.style.display = "block";
 });
 
 var flag;
 function Validator(options) {
-  function validate(inputElement, rule) {
-    var errorMessage = rule.test(inputElement.value);
-    var errorElement =
-      inputElement.parentElement.querySelector(".form-message");
-    if (errorMessage) {
-      flag = true;
-      errorElement.innerText = errorMessage;
-      inputElement.parentElement.classList.add("invalid");
-    } else {
-      flag = false;
-      errorElement.innerText = "";
-      inputElement.parentElement.classList.remove("invalid");
-    }
-  }
+	function validate(inputElement, rule) {
+		var errorMessage = rule.test(inputElement.value);
+		var errorElement =
+			inputElement.parentElement.querySelector(".form-message");
+		if (errorMessage) {
+			flag = true;
+			errorElement.innerText = errorMessage;
+			inputElement.parentElement.classList.add("invalid");
+		} else {
+			flag = false;
+			errorElement.innerText = "";
+			inputElement.parentElement.classList.remove("invalid");
+		}
+	}
 
-  var formElement1 = document.querySelector(options.form1);
-  if (formElement1) {
-    formElement1.onsubmit = function (event) {
-      event.preventDefault();
-      options.rules.forEach(function (rule) {
-        var inputElement1 = formElement1.querySelector(rule.selector);
-        validate(inputElement1, rule);
-      });
-      if (flag == false) {
-        var FullName = document.getElementById("fullname").value;
-        var Username = document.getElementById("username").value;
-        var Email = document.getElementById("email").value;
-        var Password = document.getElementById("password").value;
-        var PhoneNumber = document.getElementById("phonenumber").value;
-        var Gender = document.getElementById("gender").value;
+	var formElement1 = document.querySelector(options.form1);
+	if (formElement1) {
+		formElement1.onsubmit = function (event) {
+			event.preventDefault();
+			options.rules.forEach(function (rule) {
+				var inputElement1 = formElement1.querySelector(rule.selector);
+				validate(inputElement1, rule);
+			});
+			if (flag == false) {
+				var FullName = document.getElementById("fullname").value;
+				var Username = document.getElementById("username").value;
+				var Email = document.getElementById("email").value;
+				var Password = document.getElementById("password").value;
+				var PhoneNumber = document.getElementById("phonenumber").value;
+				var Gender = document.getElementById("gender").value;
 
-        var account = JSON.parse(localStorage.getItem("USERS")) || [];
-        var checkregister = account.find(function (user) {
-          return user.email == Email || user.username == Username;
-        });
+				var account = JSON.parse(localStorage.getItem("USERS")) || [];
+				var checkregister = account.find(function (user) {
+					return user.email == Email || user.username == Username;
+				});
 
-        if (checkregister) {
-          alert("Tên đăng nhập hoặc eamil này đã được đăng ký");
-        } else {
-          account.push({
-            fullname: FullName,
-            username: Username,
-            password: Password,
-            email: Email,
-            phonenumber: PhoneNumber,
-            gender: Gender,
-          });
+				if (checkregister) {
+					alert("Tên đăng nhập hoặc eamil này đã được đăng ký");
+				} else {
+					account.push({
+						fullname: FullName,
+						username: Username,
+						password: Password,
+						email: Email,
+						phonenumber: PhoneNumber,
+						gender: Gender,
+					});
 
-          localStorage.setItem("USERS", JSON.stringify(account));
-          alert("Đăng kí thành công");
-        }
-        document.getElementById("fullname").value = "";
-        document.getElementById("username").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("password").value = "";
-        document.getElementById("phonenumber").value = "";
-        document.getElementById("confirm").value = "";
-        document.getElementById("gender").checked = false;
-      }
-    };
+					localStorage.setItem("USERS", JSON.stringify(account));
+					alert("Đăng kí thành công");
+				}
+				document.getElementById("fullname").value = "";
+				document.getElementById("username").value = "";
+				document.getElementById("email").value = "";
+				document.getElementById("password").value = "";
+				document.getElementById("phonenumber").value = "";
+				document.getElementById("confirm").value = "";
+				document.getElementById("gender").checked = false;
+			}
+		};
 
-    options.rules.forEach(function (rule) {
-      var inputElement1 = formElement1.querySelector(rule.selector);
+		options.rules.forEach(function (rule) {
+			var inputElement1 = formElement1.querySelector(rule.selector);
 
-      if (inputElement1) {
-        inputElement1.onblur = function () {
-          validate(inputElement1, rule);
-        };
-        inputElement1.oninput = function () {
-          var errorElement =
-            inputElement1.parentElement.querySelector(".form-message");
-          errorElement.innerText = "";
-          inputElement1.parentElement.classList.remove("invalid");
-        };
-      }
-    });
-  }
+			if (inputElement1) {
+				inputElement1.onblur = function () {
+					validate(inputElement1, rule);
+				};
+				inputElement1.oninput = function () {
+					var errorElement =
+						inputElement1.parentElement.querySelector(".form-message");
+					errorElement.innerText = "";
+					inputElement1.parentElement.classList.remove("invalid");
+				};
+			}
+		});
+	}
 
-  var formElement2 = document.querySelector(options.form2);
-  if (formElement2) {
-    formElement2.onsubmit = function (event) {
-      event.preventDefault();
-      options.rules.forEach(function (rule) {
-        var inputElement2 = formElement2.querySelector(rule.selector);
-        validate(inputElement2, rule);
-      });
-      if (flag == false) {
-        var userEmail = document.getElementById("username1").value;
-        var userPassword = document.getElementById("password1").value;
+	var formElement2 = document.querySelector(options.form2);
+	if (formElement2) {
+		formElement2.onsubmit = function (event) {
+			event.preventDefault();
+			options.rules.forEach(function (rule) {
+				var inputElement2 = formElement2.querySelector(rule.selector);
+				validate(inputElement2, rule);
+			});
+			if (flag == false) {
+				var userEmail = document.getElementById("username1").value;
+				var userPassword = document.getElementById("password1").value;
 
-        account = JSON.parse(localStorage.getItem("USERS")) || [];
+				account = JSON.parse(localStorage.getItem("USERS")) || [];
 
-        var checklogin = account.find(function (user) {
-          return user.email === userEmail && user.password === userPassword;
-        });
+				var checklogin = account.find(function (user) {
+					return (
+						user.email === userEmail && user.password === userPassword
+					);
+				});
 
-        if (checklogin) {
-          alert("Đăng nhập thành công");
-        } else {
-          alert("Email hoặc mật khẩu không đúng");
-        }
-      }
+				if (checklogin) {
+					alert("Đăng nhập thành công");
+				} else {
+					alert("Email hoặc mật khẩu không đúng");
+				}
+			}
 
-      document.getElementById("username1").value = "";
-      document.getElementById("password1").value = "";
-    };
-    options.rules.forEach(function (rule) {
-      var inputElement2 = formElement2.querySelector(rule.selector);
+			document.getElementById("username1").value = "";
+			document.getElementById("password1").value = "";
+		};
+		options.rules.forEach(function (rule) {
+			var inputElement2 = formElement2.querySelector(rule.selector);
 
-      if (inputElement2) {
-        inputElement2.onblur = function () {
-          validate(inputElement2, rule);
-        };
-        inputElement2.oninput = function () {
-          var errorElement =
-            inputElement2.parentElement.querySelector(".form-message");
-          errorElement.innerText = "";
-          inputElement2.parentElement.classList.remove("invalid");
-        };
-      }
-    });
-  }
+			if (inputElement2) {
+				inputElement2.onblur = function () {
+					validate(inputElement2, rule);
+				};
+				inputElement2.oninput = function () {
+					var errorElement =
+						inputElement2.parentElement.querySelector(".form-message");
+					errorElement.innerText = "";
+					inputElement2.parentElement.classList.remove("invalid");
+				};
+			}
+		});
+	}
 }
 
 Validator.isRequired = function (selector) {
-  return {
-    selector: selector,
-    test: function (value) {
-      return value.trim() ? undefined : "Please enter information.";
-    },
-  };
+	return {
+		selector: selector,
+		test: function (value) {
+			return value.trim() ? undefined : "Please enter information.";
+		},
+	};
 };
 
 Validator.isEmail = function (selector) {
-  return {
-    selector: selector,
-    test: function (value) {
-      var isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-      return isValid ? undefined : "Please enter a valid email.";
-    },
-  };
+	return {
+		selector: selector,
+		test: function (value) {
+			var isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+			return isValid ? undefined : "Please enter a valid email.";
+		},
+	};
 };
 
 Validator.isPhoneNumber = function (selector) {
-  return {
-    selector: selector,
-    test: function (value) {
-      return /^[0-9]{10}$/.test(value)
-        ? undefined
-        : "Please enter a valid phone number";
-    },
-  };
+	return {
+		selector: selector,
+		test: function (value) {
+			return /^[0-9]{10}$/.test(value)
+				? undefined
+				: "Please enter a valid phone number";
+		},
+	};
 };
 
 Validator.isMinlength = function (selector, min) {
-  return {
-    selector: selector,
-    test: function (value) {
-      return value.length >= min
-        ? undefined
-        : "Password must contain at least  " + min + " characters.";
-    },
-  };
+	return {
+		selector: selector,
+		test: function (value) {
+			return value.length >= min
+				? undefined
+				: "Password must contain at least  " + min + " characters.";
+		},
+	};
 };
 
 Validator.isConfirm = function (selector, confirmvalue) {
-  return {
-    selector: selector,
-    test: function (value) {
-      return value === confirmvalue() ? undefined : "Password incorrect.";
-    },
-  };
+	return {
+		selector: selector,
+		test: function (value) {
+			return value === confirmvalue() ? undefined : "Password incorrect.";
+		},
+	};
 };
 
 Validator.isGenderSelected = function (selector) {
-  return {
-    selector: selector,
-    test: function (value) {
-      var genderElements = document.querySelectorAll(selector);
-      var isChecked = false;
-      for (var i = 0; i < genderElements.length; i++) {
-        if (genderElements[i].checked) {
-          isChecked = true;
-          break;
-        }
-      }
-      return isChecked ? undefined : "Please select gender.";
-    },
-  };
+	return {
+		selector: selector,
+		test: function (value) {
+			var genderElements = document.querySelectorAll(selector);
+			var isChecked = false;
+			for (var i = 0; i < genderElements.length; i++) {
+				if (genderElements[i].checked) {
+					isChecked = true;
+					break;
+				}
+			}
+			return isChecked ? undefined : "Please select gender.";
+		},
+	};
 };
