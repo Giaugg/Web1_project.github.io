f3ce1376949ee80872e32e994ce70391b27e4450
Giaugg <giaule0111@gmail.com>
2023-11-08 10:47:41 +0700

    Revert "Merge branch 'main' of https://github.com/Giaugg/Web1_project.github.io"
    This reverts commit ede4478e55d7ba6c27be3ff7e2599d8b51ce1565, reversing
    changes made to 40f577089a2d4090940fdb07cbb50975b8a6ef65.

--- Script/Script.js
+++ Script/Script.js
@@ -1,73 +1,84 @@
 var objects;
 var main_data;
 
-var api = "http://localhost:3000/Item";
+var api="http://localhost:3000/Item";
 function scrollSnapAutoScroll() {
-	const container = document.querySelector(".gallery");
-	const scrollItems = document.querySelectorAll(".gallery_item");
-	let currentIndex = 0;
+    const container = document.querySelector('.gallery');
+    const scrollItems = document.querySelectorAll('.gallery_item');
+    let currentIndex = 0;
 
-	function scrollToNextItem() {
-		currentIndex = (currentIndex + 1) % scrollItems.length;
-		container.scrollTo({
-			left: scrollItems[currentIndex].offsetLeft,
-			behavior: "smooth",
-		});
-	}
+    function scrollToNextItem() {
+        currentIndex = (currentIndex + 1) % scrollItems.length;
+        container.scrollTo({
+            left: scrollItems[currentIndex].offsetLeft,
+            behavior: 'smooth',
+        });
+    }
 
-	setInterval(scrollToNextItem, 3000); // Tự động cuộn mỗi 3 giây (3000ms)
+    setInterval(scrollToNextItem, 3000); // Tự động cuộn mỗi 3 giây (3000ms)
 }
 
 scrollSnapAutoScroll();
 ////////////// CREATE MAIN DATA////////////
 
+
 var Read_file_maindata = function () {
-	fetch(api)
-		.then((response) => response.json())
-		.then(function (data) {
-			render(data);
-		});
-};
 
-function render(courses) {
-	var itemContainer = document.getElementById("flex-container");
+    fetch(api)
+        .then(response => response.json())
+        .then(function(data){
+                  render(data);
+        });
+        
+      
+    }
 
-	var html = courses.map(function (course) {
-		console.log(course.imgage);
 
-		return `<div class="flex-item">
+function render(courses){
+    var itemContainer = document.getElementById("flex-container");
+       
+      var html=  courses.map(function(course){
+                  console.log(course.imgage)
+                 
+            return `<div class="flex-item">
             <img src="${course.imgage}" alt="" id="flex-image">
       <button class="add-to-cart-button" onclick="themvaogiohang(${course.id},'${course.name}',${course.price},'${course.imgage}')">add to cart</button>
       <p id="name">${course.name}
       <div><button class="annut" onclick="chitiet(${course.id},'${course.name}',${course.price},'${course.imgage}')">sub</button></div>
       </p>
       <p id="price">$${course.price}</p>
-      </div>`;
-	});
+      </div>`
+    
+         })
 
-	itemContainer.innerHTML += html.join("");
-}
-function themvaogiohang(id, ten, gia, hinh) {
-	var cart = JSON.parse(localStorage.getItem("cart"));
-	if (cart == null) {
-		cart = [];
-		cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
-	} else {
-		let item = cart.find((item) => item.id === id);
-		if (item) item.quality++;
-		else
-			cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
-	}
-
-	localStorage.setItem("cart", JSON.stringify(cart));
-}
-function chitiet(id, ten, gia, hinh) {
-	console.log(hinh);
-	hinh = convertImagePath(hinh);
-	var chitetElement1 = document.querySelector(".chi-tiet-modal");
-
-	var bodyElement = document.querySelector("#body");
-	chitetElement1.innerHTML = `
+         itemContainer.innerHTML+=html.join('');   
+         
+    }
+    function themvaogiohang(id,ten,gia,hinh){
+       
+       
+        var cart=JSON.parse(localStorage.getItem("cart"));
+        if(cart==null){
+          
+            cart=[];
+            cart.push({id:id,name:ten,price:gia,imgage:hinh,quality:1});
+        }
+        else{
+            let item=cart.find(item=> item.id===id);
+            if(item) item.quality++;
+            else  cart.push({id:id,name:ten,price:gia,imgage:hinh,quality:1});
+        }
+        
+        localStorage.setItem("cart",JSON.stringify(cart));
+    }
+    function chitiet(id, ten, gia, hinh) {
+        console.log(hinh)
+       hinh= convertImagePath(hinh)
+     var   chitetElement1=document.querySelector('.chi-tiet-modal')
+        
+                    var bodyElement=document.querySelector('#body')
+                  chitetElement1.innerHTML= 
+        `
                 <div class="chi-tiet-modal-container">
          <div class="modal-close" >
            <button class="annutclose">  <i class="fa-solid fa-xmark"></i></button>
@@ -102,83 +113,82 @@ function chitiet(id, ten, gia, hinh) {
              </div>
              
          </div>
-        </div>`;
-	var minusElement1 = document.querySelector("#handel-minus");
-	console.log(minusElement1);
-	var inputElement1 = document.querySelector(".fornt-sl #input");
-	console.log(inputElement1);
-	var amount1 = inputElement1.value;
+        </div>`
+        var  minusElement1=document.querySelector('#handel-minus');
+        console.log(minusElement1)
+         var inputElement1=document.querySelector('.fornt-sl #input')
+         console.log(inputElement1)
+          var amount1=inputElement1.value
+         
+          var annutCloseElement1=document.querySelector('.annutclose');
+          var giohangElement=document.querySelector('.giohangchitiet');
+          function themvaogiohang1(){
+            console.log(inputElement1.value)
+            var cart=JSON.parse(localStorage.getItem("cart"));
+        
+             if(cart==null){
+                cart=[];
+                cart.push({id:id,name:ten,price:gia,imgage:hinh,quality:inputElement1.value})
+             }
+           else{
+            let item=cart.find(item=> item.id===id);
 
-	var annutCloseElement1 = document.querySelector(".annutclose");
-	var giohangElement = document.querySelector(".giohangchitiet");
-	function themvaogiohang1() {
-		console.log(inputElement1.value);
-		var cart = JSON.parse(localStorage.getItem("cart"));
+            if(item&&inputElement1.value>=1) item.quality=inputElement1.value;
+              else cart.push({id:id,name:ten,price:gia,imgage:hinh,quality:inputElement1.value})
+           }
+           
+        
+        
+        localStorage.setItem("cart",JSON.stringify(cart));
+          }
+          giohangElement.addEventListener('click',themvaogiohang1)
+          
+             
+             annutCloseElement1.onclick=function(){
+               
+                chitetElement1.style.display="none"
+             }
+          function render2(amount)
+          {
+            inputElement1.value=amount;
+          }
+          function giatriminus(){
+            if(amount1>1)
+                 amount1--;
+    
+                 render2(amount1)
+          }
+          inputElement1.addEventListener('input',function(){
+            amount1=inputElement1.value
+          })
+          function giatriplus(){
+                 amount1++;
+                 render2(amount1)
+          }
+          var plusElement1 = document.querySelector('#handel-plus');
+      plusElement1.addEventListener('click', giatriplus);
+      minusElement1.addEventListener('click',giatriminus)
 
-		if (cart == null) {
-			cart = [];
-			cart.push({
-				id: id,
-				name: ten,
-				price: gia,
-				imgage: hinh,
-				quality: inputElement1.value,
-			});
-		} else {
-			let item = cart.find((item) => item.id === id);
+                    chitetElement1.style.display="flex"
+      
+                   
 
-			if (item && inputElement1.value >= 1)
-				item.quality = inputElement1.value;
-			else
-				cart.push({
-					id: id,
-					name: ten,
-					price: gia,
-					imgage: hinh,
-					quality: inputElement1.value,
-				});
-		}
-
-		localStorage.setItem("cart", JSON.stringify(cart));
-	}
-	giohangElement.addEventListener("click", themvaogiohang1);
-
-	annutCloseElement1.onclick = function () {
-		chitetElement1.style.display = "none";
-	};
-	function render2(amount) {
-		inputElement1.value = amount;
-	}
-	function giatriminus() {
-		if (amount1 > 1) amount1--;
-
-		render2(amount1);
-	}
-	inputElement1.addEventListener("input", function () {
-		amount1 = inputElement1.value;
-	});
-	function giatriplus() {
-		amount1++;
-		render2(amount1);
-	}
-	var plusElement1 = document.querySelector("#handel-plus");
-	plusElement1.addEventListener("click", giatriplus);
-	minusElement1.addEventListener("click", giatriminus);
-
-	chitetElement1.style.display = "flex";
+             
+           
+      
 }
 function convertImagePath(inputPath) {
-	// Sử dụng phương thức replace để thay thế chuỗi "..Images" thành "..\Images\products\"
-	const convertedPath = inputPath.replace(
-		"..Imagesproducts",
-		"..\\Images\\products\\"
-	);
-	return convertedPath;
-}
-
-// Sử dụng hàm với ví dụ
-const inputPath = "..ImagesproductsID_Products004.jpg";
-const convertedPath = convertImagePath(inputPath);
-console.log(convertedPath);
+    // Sử dụng phương thức replace để thay thế chuỗi "..Images" thành "..\Images\products\"
+    const convertedPath = inputPath.replace("..Imagesproducts", "..\\Images\\products\\");
+    return convertedPath;
+  }
+  
+  // Sử dụng hàm với ví dụ
+  const inputPath = "..ImagesproductsID_Products004.jpg";
+  const convertedPath = convertImagePath(inputPath);
+  console.log(convertedPath);
+  
 
 Read_file_maindata();
+
+
