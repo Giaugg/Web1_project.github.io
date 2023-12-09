d7ec9b82c83b464c3957c2a3daaf8b1d10f9674f
tandat-cloud <149981466+tandat-cloud@users.noreply.github.com>
2023-12-09 13:34:32 +0700

    Update Script.js

--- Script/Script.js
+++ Script/Script.js
@@ -268,12 +268,12 @@ function themvaogiohang(id, ten, gia, hinh) {
 		var cart = JSON.parse(localStorage.getItem("cart"));
 		if (cart == null) {
 			cart = [];
-			cart.push({ id: id, name: ten, price: gia, image: hinh, quality: 1 });
+			cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
 		} else {
 			let item = cart.find((item) => item.id === id);
 			if (item) item.quality++;
 			else
-				cart.push({ id: id, name: ten, price: gia, image: hinh, quality: 1 });
+				cart.push({ id: id, name: ten, price: gia, imgage: hinh, quality: 1 });
 		}
 
 		localStorage.setItem("cart", JSON.stringify(cart));
